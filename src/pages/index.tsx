c00df515ec9c760da4107f57ffa1c738688229e7
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2022-11-18 21:54:17 -0300

    feat: :sparkles: fetch products from api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { FooterMessage } from '../components/atoms/FooterMessage'
+
 import { Gallery } from '../components/organisms/Gallery'
 import { Header } from '../components/organisms/Header'
+import { api } from '../services/api'
+import { Product } from '../types/Product'
 
-export default function Home() {
+interface HomeProps {
+  products: Product[]
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <div>
       <Head>
@@ -14,9 +23,29 @@ export default function Home() {
       <Header />
       <main>
         <div className="pt-9">
-          <Gallery title="Recommended" />
+          <Gallery title="Recommended" products={products} />
         </div>
       </main>
+      <FooterMessage />
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  try {
+    const response = await api.get('/products?limit=6')
+
+    return {
+      props: {
+        products: response.data,
+      },
+      revalidate: 60 * 60 * 12, // 12 hours
+    }
+  } catch {
+    return {
+      props: {
+        products: [],
+      },
+    }
+  }
 }
