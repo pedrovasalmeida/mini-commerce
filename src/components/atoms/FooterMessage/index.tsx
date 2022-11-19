c63709a034470b03c72d24db518d6ba5fd88bede
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2022-11-18 21:54:28 -0300

    feat: :sparkles: create footer message with personal email

---
+++ src/components/atoms/FooterMessage/index.tsx
@@ -0,0 +1,8 @@
+export function FooterMessage() {
+  return (
+    <div className="absolute bottom-0 right-0 left-0 flex items-center justify-center gap-2 bg-stone-100 pb-1 w-screen">
+      <div className="w-8 h-px bg-gray-400 shadow" />
+      <p className="text-xs text-gray-400">pedrovasalmeida@gmail.com</p>
+    </div>
+  )
+}
