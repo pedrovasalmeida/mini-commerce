26d0b47f88b457fcb772c45156ba437382d833f9
Pedro Henrique de V. A <pedrovasalmeida@gmail.com>
2022-11-21 23:06:41 -0300

    feat: :sparkles: add product quantity handler

--- src/components/molecules/ProductQuantitySelector/index.tsx
+++ src/components/molecules/ProductQuantitySelector/index.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react'
+
 export function ProductQuantitySelector() {
+  const [quantity, setQuantity] = useState(1)
+
+  const handleIncrement = () => {
+    setQuantity((current) => current + 1)
+  }
+
+  const handleDecrement = () => {
+    setQuantity((current) => (current <= 1 ? 1 : current - 1))
+  }
+
   return (
     <div className="flex w-auto items-center justify-center gap-2 bg-zinc-200 rounded-lg p-1">
-      <div className="p-1 px-3 border border-gray-400 bg-gray-100 rounded-lg">
+      <button
+        className="p-1 px-3 border border-gray-400 bg-gray-100 rounded-lg"
+        onClick={handleDecrement}
+      >
         <p>-</p>
-      </div>
-      <span className="font-bold text-md">2</span>
-      <div className="p-1 px-3 border border-gray-400 bg-gray-100 rounded-lg">
+      </button>
+      <span className="font-bold text-md">{quantity}</span>
+      <button
+        className="p-1 px-3 border border-gray-400 bg-gray-100 rounded-lg"
+        onClick={handleIncrement}
+      >
         <p>+</p>
-      </div>
+      </button>
     </div>
   )
 }
